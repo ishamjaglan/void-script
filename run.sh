4e8b7d75608ed024d8431ac6f9aca1c2ef2ba2c5
ixh4m <ishamjaglan@gmail.com>
2022-07-23 20:09:32 +0530

    added new features

--- run.sh
+++ run.sh
@@ -4,7 +4,15 @@ echo "Follow me on GitHub:"
 echo "https://github.com/ixh4m"
 mkdir downloaded-packages
 cd downloaded-packages
-sudo xbps-install curl
+sudo xbps-install -Syu axel void-repo-multilib void-repo-nonfree void-repo-multilib-nonfree flatpak
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+export PATH=$PATH:/var/lib/flatpak/exports/share:/home/$USER/.local/share/flatpak/exports/share
+echo "export PATH=$PATH:/var/lib/flatpak/exports/share:/home/$USER/.local/share/flatpak/exports/share" >> ~/.bash_profile
+echo "export PATH=$PATH:/var/lib/flatpak/exports/share:/home/$USER/.local/share/flatpak/exports/share" >> ~/.zshenv
+source ~/.bash_profile
+source ~/.zshenv
+flatpak install -y flathub
+sudo xbps-install -Syu
 echo -e "\nWant to install Discord?"
 PS3="Please select an option : "
 choices=("yes" "no")
@@ -13,13 +21,27 @@ select choice in "${choices[@]}"; do
           yes)
 
             echo -e "\nInstalling Discord"
-            curl -L "https://discord.com/api/download?platform=linux&format=tar.gz" -o "discord.tar.gz"
-            sudo xbps-install -S libatomic
-            sudo tar -xvzf discord-0.0.10.tar.gz -C /opt
-            sudo ln -sf /opt/Discord/Discord /usr/bin/Discord
-            sudo chmod +x /opt/Discord/discord.desktop
-            sudo cp /opt/Discord/discord.desktop /usr/share/applications/discord.desktop
+	    flatpak install -y com.discordapp.Discord
+	    sleep 1
+            break
+            ;;
+          no)
+            echo -e "Nise."
             sleep 1
+            break
+            ;;
+  esac
+done
+echo -e "\nWant to install LibreOffice?"
+PS3="Please select an option : "
+choices=("yes" "no")
+select choice in "${choices[@]}"; do
+        case $choice in
+          yes)
+
+            echo -e "\nInstalling LibreOffice"
+	    sudo xbps-install -y libreoffice
+	    sleep 1
             break
             ;;
           no)
@@ -37,7 +59,7 @@ select choice in "${choices[@]}"; do
           yes)
 
             echo -e "\nInstalling VSCode"
-            sudo xbps-install -S vscode
+            sudo xbps-install -Sy vscode
             sleep 1
             break
             ;;
@@ -56,8 +78,7 @@ select choice in "${choices[@]}"; do
           yes)
 
             echo -e "\nInstalling Steam"
-            sudo xbps-install -Sy void-repo-nonfree void-repo-multilib
-            sudo xbps-install -S steam libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit mesa-dri-32bit mono
+            sudo xbps-install -Sy steam libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit mesa-dri-32bit mono
             sleep 1
             break
             ;;
@@ -80,11 +101,7 @@ select choice in "${choices[@]}"; do
             sudo xbps-install -Sy nmap binwalk sqlmap wireshark ghidra john netcat hashcat thc-hydra ruby rust go sqlite nodejs python3 python3-pip wget gcc ruby-devel libxml2 libxml2-devel libxslt libxslt-devel inetutils
             
             #rubygems
-            curl -L "https://rubygems.org/rubygems/rubygems-3.2.21.tgz" -o "rubygems.tgz"
-            tar -xvzf rubygems.tgz
-            cd rubygems-3.2.21
-            sudo ruby setup.rb
-            cd ..
+	    sudo xbps-install -y ruby-devel
 
             #wpscan
             sudo gem install nokogiri
@@ -110,10 +127,10 @@ select choice in "${choices[@]}"; do
 
 
             #metasploit framework
-            curl -L "http://downloads.metasploit.com/data/releases/metasploit-latest-linux-installer.run" -o "msfinstall"
-            chmod +x msfinstall
-            sudo ./msfinstall
-
+            axel "http://downloads.metasploit.com/data/releases/metasploit-latest-linux-installer.run"
+            chmod +x metasploit-latest-linux-installer.run
+            sudo ./metasploit-latest-linux-installer.run
+	    rm metasploit-latest-linux-installer.run
             sleep 1
             break
             ;;
@@ -124,6 +141,88 @@ select choice in "${choices[@]}"; do
             ;;
   esac
 done
+echo -e "\nWant to install Xtreme Download Manager?"
+PS3="Please select an option : "
+choices=("yes" "no")
+select choice in "${choices[@]}"; do
+        case $choice in
+          yes)
+
+            echo -e "\nInstalling Xtreme Download Manager"
+            axel "https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz" -o "xdm.tar.xz"
+	    tar -xf xdm.tar.xz
+	    sudo ./install.sh
+	    rm install.sh readme.sh xdm.tar.xz
+	    sleep 1
+            break
+            ;;
+          no)
+            echo -e "Nise."
+            sleep 1
+            break
+            ;;
+  esac
+done
+echo -e "\nWant to install Spotify?"
+PS3="Please select an option : "
+choices=("yes" "no")
+select choice in "${choices[@]}"; do
+        case $choice in
+          yes)
+
+            echo -e "\nInstalling Spotify"
+	    flatpak install -y com.spotify.Client
+	    sleep 1
+            break
+            ;;
+          no)
+            echo -e "Nise."
+            sleep 1
+            break
+            ;;
+  esac
+done
+echo -e "\nWant to install Bitwarden?"
+PS3="Please select an option : "
+choices=("yes" "no")
+select choice in "${choices[@]}"; do
+        case $choice in
+          yes)
+
+            echo -e "\nInstalling Spotify"
+	    flatpak install -y com.bitwarden.desktop
+	    sleep 1
+            break
+            ;;
+          no)
+            echo -e "Nise."
+            sleep 1
+            break
+            ;;
+  esac
+done
+
+echo -e "\nWant to install QEMU?"
+PS3="Please select an option : "
+choices=("yes" "no")
+select choice in "${choices[@]}"; do
+        case $choice in
+          yes)
+
+            echo -e "\nInstalling QEMU and Virt-Manager"
+            sudo xbps-install -y qemu virt-manager
+
+            sleep 1
+            break
+            ;;
+          no)
+            echo -e "Nise."
+            sleep 1
+            break
+            ;;
+  esac
+done
+
 cd .. && rm -rf downloaded-packages
 echo -e "\n\n\nScript made by Isham Jaglan"
 echo "Follow me on GitHub:"
